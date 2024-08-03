8902c59b3bbc4c4ada813db6dfaf6ce72348493c
Mina <minaelhamy@gmail.com>
2024-08-03 22:28:32 +0300

    import clerk duplicate

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ import ImageKit from "imagekit";
 import mongoose from "mongoose";
 import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
-import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 3000;
